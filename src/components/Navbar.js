f83fccc16d14e60d3e45d3b70680a1f1eee76aa7
SofiFerrarotti <106496505+SofiFerrarotti@users.noreply.github.com>
2024-10-31 23:34:53 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Asegúrate de esta línea
+import { Link } from 'react-router-dom'; 
 import './Navbar.css';
 
 const Navbar = () => {
